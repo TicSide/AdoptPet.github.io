61d13e35bddf0622f91ed844504d79b0fcae8c2d
Harold Ticllacuri <omarriveraticllacuri@gmail.com>
2023-11-20 17:41:24 -0500

    Last Code

---
+++ assets/scripts/Mascotas.js
@@ -0,0 +1,52 @@
+
+
+    const services = document.querySelector("#services");
+    const portafolio = document.querySelector("#portafolio");
+    const faq = document.querySelector("#faq");
+    const buttonA = document.querySelector(".checkbtn");
+    const menu =document.querySelector(".MenuDesplegableVisible");
+
+    buttonA.addEventListener("click", () => {
+        menu.classList.toggle("MenuDesplegableVisible");
+
+        if (menu.classList.contains("nav-MenuDesplegableVisible")) {
+            buttonA.setAttribute("aria-label", "Cerrar menú");
+        } else {
+            buttonA.setAttribute("aria-label", "Abrir menú");
+        }
+    });
+
+
+
+
+
+
+    services.addEventListener("click", (s) => {
+        s.preventDefault();
+        const sectionS = document.querySelector(".services");
+        sectionS.scrollIntoView({behavior: "smooth"});
+    })
+    portafolio.addEventListener("click", (p) => {
+        p.preventDefault();
+        const sectionP = document.querySelector(".portafolio");
+        sectionP.scrollIntoView({behavior: "smooth"});
+    })
+
+    faq.addEventListener("click", (p) => {
+        p.preventDefault();
+        const sectionP = document.querySelector(".faq");
+        sectionP.scrollIntoView({behavior: "smooth"});
+    })
+
+    let processScroll = () => {
+        let docElem = document.documentElement,
+            docBody = document.body,
+            scrollTop = docElem['scrollTop'] || docBody['scrollTop'],
+            scrollBottom = (docElem['scrollHeight'] || docBody['scrollHeight']) - window.innerHeight,
+            scrollPercent = scrollTop / scrollBottom * 100 + '%';
+
+        // console.log(scrollTop + ' / ' + scrollBottom + ' / ' + scrollPercent);
+
+        document.getElementById("progress-bar").style.setProperty("--scrollAmount", scrollPercent);
+    }
+    document.addEventListener('scroll', processScroll);
